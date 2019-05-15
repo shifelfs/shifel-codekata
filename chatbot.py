bfe5faee2410d8a1c28c40cc0b6efb53aa2eba7a
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-05-15 16:53:16 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+a=["welcome","what is your name","no.of guest","thank you","ur booking is confirmed"]
+user=str(input("user: "))
+
+if (user=='hello'):
+  print("bot:",a[0])
+  print(a[1])
+  
+  b=str(input("user: "))
+  print("bot:",a[2])
+
+  c=str(input("user: "))
+  print("bot:",a[3],b)
+  print("no.of guest",c)
+  print(a[4])
+  
