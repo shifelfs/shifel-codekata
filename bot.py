02072d62f9f6dd4f0ebb124ea2dc1f5c88618df6
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-05-15 16:44:49 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+a=["welcome","what is your name","no.of guest","thank you","ur booking is confirmed"]
+user=str(input())
+if (user=='hello'):
+  print(a[0])
+  print(a[1])
+  b=str(input())
+  print(a[2])
+  c=str(input())
+  print(a[3],b)
+  print("no.of guest",c)
+  print(a[4])
+  
